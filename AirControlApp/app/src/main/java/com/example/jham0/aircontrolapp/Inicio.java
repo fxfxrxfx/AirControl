145dc48bbbeaee3e1751fa227765fe511ace1483
JQartas <jcuartas0611@gmail.com>
2018-10-09 19:26:37 -0500

    1.0

--- AirControlApp/app/src/main/java/com/example/jham0/aircontrolapp/Inicio.java
+++ AirControlApp/app/src/main/java/com/example/jham0/aircontrolapp/Inicio.java
@@ -1,13 +1,34 @@
 package com.example.jham0.aircontrolapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class Inicio extends AppCompatActivity {
+public class inicio extends AppCompatActivity {
+
+    //Components
+    LinearLayout btnTools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        connect();
+        onClickButtons();
+    }
+
+    private void connect() {
+        btnTools = findViewById(R.id.ly_tools);
+    }
+
+    public void onClickButtons() {
+        btnTools.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),tools.class));
+            }
+        });
     }
 }
---
+++ AirControlApp/app/src/main/java/com/example/jham0/aircontrolapp/tools.java
@@ -0,0 +1,13 @@
+package com.example.jham0.aircontrolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tools extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tools);
+    }
+}
