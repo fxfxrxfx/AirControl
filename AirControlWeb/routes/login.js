3e0ddb9dc8b01eb20c3834e4b5f8e725ece8ebad
Unknown <santi-c-b@outlook.com>
2018-10-14 18:24:22 -0500

    Web
    Rama creada solo para el deploy del servidor web

--- AirControlWeb/routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET stat graphics. */
-router.get('/', function(req, res, next) {
-  res.render('login', {nombre: "oa"});
-});
-
-module.exports = router;
--- AirControlWeb/routes/stats.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET stat graphics. */
-router.get('/', function(req, res, next) {
-  res.render('stats', {nombre: "oa"});
-});
-
-module.exports = router;
